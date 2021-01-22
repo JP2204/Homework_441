d93c7b9d065d3d9636e5238f55f825b0a4c3efb8
JP2204 <james.pierce@umontana.edu>
2021-01-22 17:16:07 -0500

    push

---
+++ HW2/script.js
@@ -0,0 +1,20 @@
+//Lets begin
+
+console.log("My Favorite Websites")
+console.log("1: Youtube")// spend way to much time watching videos
+console.log("2: Crunchyroll")// love Anime
+console.log("3: StockX")// I love hype shoes and street wear
+
+console.log(" ")// Space
+
+console.log("My Favorite Games")
+console.log("1: Rocket League")// me and this game have a love hate relationship
+console.log("2: Batman Arkham Series")// Really Love Batman
+console.log("3:Star Wars Jedi Fallen Order")// This Games Lets Me Be a Jedi, Whats Not To love
+
+console.log(" ")//Space
+
+console.log("My Favorite Artist's")
+console.log("1: Juice wrld")// his Music speaks to me RIP
+console.log("2: KAWS")// I love his Artwork and the impact it has on street culture
+console.log("3: Takashi Murakami")// I love his work for the same reason as KAWS
