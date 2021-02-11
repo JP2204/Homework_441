30c244a9a340f41175fc4447ff1c62d1999b4bdf
JP2204 <james.pierce@umontana.edu>
2021-02-11 15:21:05 -0500

    push

--- HW5/script.js
+++ HW5/script.js
@@ -1,57 +1,60 @@
-// create an array of image names that correspond to the image tags
-var imageTags = ["image1", "image2", "image3", "image4"];
-// create a variable with the blank image name
-var blankImagePath = "images/gofish.jpg";
-// create a empty array for the actual images
-var actualImages = new Array();
+var cardIDs = ["card1","card2","card3","card4","card5","card6","card7","card8","card9","card10"];
+var blankImage = "images/question.jpg";
+var images = new Array();
+var flips = 0;
+var flipIndices = [0,0];
+var score = 0;
 
-function printBlanks()
-{
-   // call our random image creation function
-    createRandomImageArray();
-    // create a for loop
-    for(var i = 0; i < imageTags.length; i++)
-    {
-    // iterate through the image tag ids and sets the source
-        document.getElementById(imageTags[i]).src= blankImagePath;
-    }
-
-
-
+function displayBlanks() {
+    createRandomArray();
 }
 
-function createRandomImageArray()
-{
-    // create an array of actual images
-    var actualImagePath = ["images/dog.jpg", "images/tiger.jpeg"];
-    // create another array to make sure the images only get added twice
-    var count = [0,0];
-    // create a while statement to check to see if our actual image array is full
-    while(actualImages.length < 4)
-    {
-        // get a random number between 0 and the number total number of images that we can choose from
-        var randomNumber = Math.floor(Math.random() * actualImagePath.length)
-          // create an if statement that says if the total number added is less than 2, then
-        // add the image to the actual image array
-        if(count[randomNumber] < 2)
-        {
-            actualImages.push(actualImagePath[randomNumber]);
-            // then add one to the array that makes sure only two images can be added
-            count[randomNumber] = count[randomNumber] + 1;
+function createRandomArray() {
+    var imagePaths = ["images/anemone.jpg","images/dahlia.jpg","images/lavender.jpg","images/rose.jpg","images/tulip.jpg"];
+    var imageDeckCount = [0,0,0,0,0];
+
+    while (images.length < 10) {
+        var index  = Math.floor(Math.random() * imagePaths.length)
+        if (imageDeckCount[index] < 2) {
+            images.push(imagePaths[index]);
+            imageDeckCount[index] += 1;
         }
     }
-
-
-
-
-
 }
 
-function flipImage(number)
-{
-    document.getElementById(imageTags[number]).src= actualImages[number];
-        // this should be a quick function that just changes
-        // the image based on what number was pressed
+function flipCard(index) {
+    if (flips == 2) { checkForMatch(); }
+    else {
+        document.getElementById(cardIDs[index]).src = images[index];
+        flipIndices[flips] = index;
+        flips++;
+    }
+}
 
+function resetFlippedCards() {
+    document.getElementById(cardIDs[flipIndices[0]]).src = blankImage;
+    document.getElementById(cardIDs[flipIndices[1]]).src = blankImage;
+}
 
+function checkForMatch() {
+    if (images[flipIndices[0]] == images[flipIndices[1]]) {
+        score += 1;
+        document.getElementById('score').innerHTML = "Score: " + score;
+        disableClickOnMatch();
+        resetCount();
+    }
+    else {
+        resetFlippedCards();
+        resetCount();
+    }
+}
+
+function disableClickOnMatch() {
+    document.getElementById(cardIDs[flipIndices[0]]).onclick = null;
+    document.getElementById(cardIDs[flipIndices[1]]).onclick = null;
+}
+
+function resetCount() {
+    flips = 0;
+    flipIndices = [0,0];
 }
